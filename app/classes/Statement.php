833a118b90d957c14eeb425f6ef0db50a928c97b
shakhawatsabbir <mdshakhawathossain2001@gmail.com>
2022-10-23 01:02:07 +0600

    ifelseif statement work test

--- app/classes/Statement.php
+++ app/classes/Statement.php
@@ -12,9 +12,41 @@ class Statement
         $this->z = 30;
     }
 
+
+
+
     public function index()
     {
 
+        //<---------- ifelseif statement working ------------>//
+
+//        if ($this->x > $this->y)
+//        {
+//            echo "hello King";
+//        }
+//        elseif ($this->y < $this->z)
+//        {
+//            echo "Hello World";
+//        }
+//        elseif ($this->x < $this->z)
+//        {
+//            echo "Hello Bangladesh";
+//        }
+//        else
+//        {
+//            echo "Nothing";
+//        }
+
+        //<---------- ifelse statement working ------------>//
+
+//        if ($this->x > $this->y)
+//        {
+//            echo "hello World";
+//        }
+//        else
+//        {
+//            echo "Nothing";
+//        }
 
 
         //<---------- if statement working ------------>//
