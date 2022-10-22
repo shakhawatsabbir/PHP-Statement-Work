80c9d5193e504eeb5852a0465e4b4a0499534b96
shakhawatsabbir <mdshakhawathossain2001@gmail.com>
2022-10-23 00:23:40 +0600

    php  Statement work

---
+++ app/classes/Statement.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\classes;
+
+class Statement
+{
+    public $x, $y , $z;
+    public function __construct()
+    {
+//        $this->x = 10;
+//        $this->y = 20;
+//        $this->z = 30;
+    }
+
+    public function index()
+    {
+
+
+
+
+        //<---------- while statement working ------------>//
+
+//        $this->x = 10;
+//        while ($this->x < 10004)
+//        {
+//            echo $this->x. "  ";
+//            $this->x++;
+//        }
+
+
+        //<---------- For statement working ------------>//
+
+
+
+//        for ($this->z = 10; $this->z < 14; $this->z++)
+//            {
+//                echo $this->z. "  ";
+//            }
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once "vendor/autoload.php";
+use App\classes\Statement;
+
+$hello= new Statement();
+$hello->index();
